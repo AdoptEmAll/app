b3bb77ad3c3a683be20aeaf64b4509ff918f8cf7
Chad Manuel <chadc_manuel@yahoo.com>
2023-10-04 13:39:37 -0700

    Connected to db

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+console.log(process.env.MONGODB_URL)
+
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(()=> {
+        console.log('App has connected to database');
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(`Error connection to the db: ${error}`);
+    })
