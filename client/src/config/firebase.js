9568b911e5f8804496e805f47ab9c81596520ccd
Miguel Robles <migarob@icloud.com>
2023-10-11 13:23:41 -0700

    added Firebase before firebase-tools (CLI)

---
+++ client/src/config/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkkk3vEaPnOcisoKoxB_QkH1CWKUgbbo0",
+  authDomain: "adoptemall.firebaseapp.com",
+  projectId: "adoptemall",
+  storageBucket: "adoptemall.appspot.com",
+  messagingSenderId: "198044004852",
+  appId: "1:198044004852:web:da1880cfae5532a8a949a1",
+  measurementId: "G-3EZHGLCE65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
